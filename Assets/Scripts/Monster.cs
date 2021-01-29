8c01e14aaae29767d4ebeb734bd4db9c7c9fbf45
Noe Rivals <noe.rivals@gmail.com>
2021-01-29 22:57:44 +0000

    Add monster script

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    public float visionRange = 10;
+    public float visionAngle = 90;
+    public float hearingRatio = 1;
+
+    private GameObject player;
+    private PlayerStats playerStats;
+    private NavMeshAgent navMeshAgent;
+    private Vector3 target = new Vector3(0, 0, 0);
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+        {
+            print("Error: no player found");
+        }
+        playerStats = player.GetComponent<PlayerStats>();
+    }
+
+    Vector3 getDefaultTarget()
+    {
+        return new Vector3(0, 0, 0);
+    }
+
+    void updateTarget()
+    {
+        Vector3 playerDirection = player.transform.position - transform.position;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, playerDirection, out hit, visionRange) &&
+            hit.collider.gameObject.CompareTag("Player") &&
+            Vector3.Angle(transform.forward, playerDirection.normalized) <= visionAngle / 2.0f
+        )
+        {
+            target = hit.point;
+            return;
+        }
+
+        if (playerStats.getSoundDistance() * hearingRatio >= playerDirection.magnitude)
+        {
+            target = player.transform.position;
+        }
+
+        if ((target - transform.position).magnitude <= 0.1)
+        {
+            target = getDefaultTarget();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateTarget();
+        navMeshAgent.SetDestination(target);
+        // transform.position += (target - transform.position).normalized * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float soundDistanceRunning = 20;
+    public float soundDistanceWalking = 10;
+    public bool running = false;
+    public bool moving = true;
+    public float getSoundDistance()
+    {
+        if (!moving) return 0;
+        return running ? soundDistanceRunning : soundDistanceWalking;
+    }
+}
