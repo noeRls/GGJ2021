c21b35f5afd43a277587f3f73c3f2e892810041d
Noe Rivals <noe.rivals@gmail.com>
2021-01-30 13:39:03 +0000

    Add pig behavior

--- Assets/Scripts/Monsters/Pig.cs
+++ Assets/Scripts/Monsters/Pig.cs
@@ -2,18 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pig : MonoBehaviour
+public class Pig : Monster
 {
-    private Monster monster;
-    // Start is called before the first frame update
-    void Start()
+    public float chargeSetupTime = 1;
+    private float chargeSetupTimeout = 0;
+
+    public float chargeInterval = 5;
+    private float chargeTimeout = 0;
+
+    public float speedCharge = 10;
+    private bool isCharging = false;
+
+    override protected void updateTarget()
     {
-        
+        if (isCharging)
+        {
+            return;
+        }
+        if (!playerPos.HasValue || chargeTimeout > 0)
+        {
+            base.updateTarget();
+            return;
+        }
+        isCharging = true;
+        chargeTimeout = chargeInterval;
+        chargeSetupTimeout = chargeSetupTime;
+        target = playerPos.Value;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void onTargetReach()
     {
-        
+        if (isCharging)
+        {
+            isCharging = false;
+            updateTarget();
+        } else
+        {
+            base.onTargetReach();
+        }
+    }
+
+    protected override void setSpeed()
+    {
+        if (isCharging)
+        {
+            if (chargeSetupTimeout > 0)
+            {
+                navMeshAgent.speed = 0;
+            } else
+            {
+                navMeshAgent.speed = speedCharge;
+            }
+        } else
+        {
+            base.setSpeed();
+        }
+    }
+
+    private void Update()
+    {
+        monsterUpdate();
+        chargeSetupTimeout -= Time.deltaTime;
+        chargeTimeout -= Time.deltaTime;
     }
 }
