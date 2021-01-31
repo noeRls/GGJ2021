5f5b9ee6d5cb584d2afb66535a6dd3dff727f53a
PrideTheDaemon <louis.prudhomme@hotmail.com>
2021-01-31 04:38:51 +0000

    Added puse menu (weird bug with return button not accessible after shop)

--- Assets/Scripts/UIManagers/PauseManager.cs
+++ Assets/Scripts/UIManagers/PauseManager.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
+    public GuiManager guiManager;
+
+    public Button quitButton;
+    public Button resumeButton;
+    public Button startOverButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        quitButton.onClick.AddListener(OnButtonExitClick);
+        resumeButton.onClick.AddListener(OnButtonResumeClick);
+        startOverButton.onClick.AddListener(OnButtonStartOverClick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnButtonExitClick()
+    {
+        //todo
+    }
+
+    private void OnButtonResumeClick()
+    {
+        guiManager.TogglePause();
+    }
+
+    private void OnButtonStartOverClick()
+    {
+        //todox
     }
 }
